778db75116f196dc1139a8920513b34a16f53ba0
Javi <rtomyj@gmail.com>
2023-04-23 13:58:35 -0500

    Removed unused plugins

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+	addons: ['@storybook/addon-essentials'],
 	framework: {
 		name: '@storybook/react-webpack5',
 		options: {},
