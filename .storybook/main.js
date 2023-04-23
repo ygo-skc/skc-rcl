eb15a56797f0268486a86025d7b993e40d76c798
Javi <rtomyj@gmail.com>
2023-04-22 22:19:17 -0500

    removed uneeded module

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: {
 		name: '@storybook/react-webpack5',
 		options: {},
