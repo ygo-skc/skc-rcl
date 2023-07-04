0480229928572ca6ef670c3608eda163fe6fe75d
Javi <rtomyj@gmail.com>
2023-07-03 19:15:29 -0500

    Updated styles

--- src/components/data-display/InlineDate.tsx
+++ src/components/data-display/InlineDate.tsx
@@ -6,15 +6,15 @@ import '../../css/data-display/inline-date.css'
 const InlineDate: FC<DateProps> = ({ month, day, year }) => {
 	return (
 		<div className='inline-date '>
-			<Typography data-testid='date-component-month-text' variant='h5' align='center' className='month-text'>
+			<Typography data-testid='date-component-month-text' variant='h5' align='center' className='text month-text'>
 				{month}
 			</Typography>
 
-			<Typography data-testid='date-component-day-text' align='center' variant='h6' className='day-text'>
+			<Typography data-testid='date-component-day-text' align='center' variant='h6' className='text day-text'>
 				{day}
 			</Typography>
 
-			<Typography data-testid='date-component-year-text' align='center' variant='subtitle1'>
+			<Typography data-testid='date-component-year-text' align='center' variant='subtitle1' className='text'>
 				{year}
 			</Typography>
 		</div>
