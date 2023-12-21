400dcfb909b8eef7442b9511e4f517cf0d53e910
Javi <8519394+rtomyj@users.noreply.github.com>
2023-12-20 20:18:39 -0600

    Fixing sonar warning

--- src/components/data-display/Tile.tsx
+++ src/components/data-display/Tile.tsx
@@ -7,20 +7,20 @@ export type TileProps = {
 	total: number
 	subject: string
 	color: string
-	action: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+	action: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 	variant: 'small' | 'medium' | 'large' | 'full-width'
 }
 
 const Tile: FC<TileProps> = ({ total, subject, color, action, variant }) => {
 	return (
-		<div data-testid='tile-component' className={`tile ${variant}`} onClick={action}>
+		<button data-testid='tile-component' className={`tile ${variant}`} onClick={action}>
 			<Typography data-testid='tile-component-prominent-text' className='tile-prominent-text' variant='h1' align='center'>
 				{total.toLocaleString('en')}
 			</Typography>
 			<Typography data-testid='tile-component-sub-text' variant='h5' align='center' style={{ color: color }}>
 				{subject}
 			</Typography>
-		</div>
+		</button>
 	)
 }
 
